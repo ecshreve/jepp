182fe6b40b22f5366f1f610bbe1ded2ca2c63dfc
shreve <eric@shreve.dev>
2023-06-23 10:34:20 +0000

    fix: update scheme to https

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,12 +12,13 @@ import (
 //	@description	This is a simple api to access jeopardy data.
 //	@version		1.0
 //	@basepath		/api
-//	@schemes		http
+//	@schemes		https
 //	@contact.name	shreve
 //	@license.name	MIT
 //	@license.url	https://github.com/ecshreve/jepp/blob/main/LICENSE
 func main() {
 	docs.SwaggerInfo.Host = os.Getenv("API_HOST")
+
 	log.SetLevel(log.DebugLevel)
 	log.Info("Starting Jepp API server...")
 
