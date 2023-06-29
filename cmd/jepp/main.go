bc850b3fd78053d85f04a30eee89ffc000e8529f
shreve <eric@shreve.dev>
2023-06-29 07:09:37 +0000

    feat: bootstrap gql

--- cmd/jepp/main.go
+++ cmd/jepp/main.go
@@ -1,32 +1,61 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"os"
 
-	"github.com/ecshreve/jepp/docs"
-	"github.com/ecshreve/jepp/pkg/server"
-	log "github.com/sirupsen/logrus"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/ecshreve/jepp/graph"
 )
 
-// @title			Jepp API Documentation
-// @description	This is a simple api to access jeopardy data.
-// @version		1.0
-// @BasePath		/api
-//
-// @contact.name	shreve
-//
-// @license.name	MIT License
-// @license.url	https://github.com/ecshreve/jepp/blob/main/LICENSE
+const defaultPort = "8080"
+
 func main() {
-	if os.Getenv("JEPP_ENV") == "prod" {
-		docs.SwaggerInfo.Host = "jepp.app"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
 	}
 
-	log.SetLevel(log.DebugLevel)
-	log.Info("Starting Jepp API server...")
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	srv := server.NewServer()
-	if err := srv.Router.Run(":8880"); err != nil {
-		log.Fatal(err)
-	}
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
+
+// package main
+
+// import (
+// 	"os"
+
+// 	"github.com/ecshreve/jepp/docs"
+// 	"github.com/ecshreve/jepp/pkg/server"
+// 	log "github.com/sirupsen/logrus"
+// )
+
+// // @title			Jepp API Documentation
+// // @description	This is a simple api to access jeopardy data.
+// // @version		1.0
+// // @BasePath		/api
+// //
+// // @contact.name	shreve
+// //
+// // @license.name	MIT License
+// // @license.url	https://github.com/ecshreve/jepp/blob/main/LICENSE
+// func main() {
+// 	if os.Getenv("JEPP_ENV") == "prod" {
+// 		docs.SwaggerInfo.Host = "jepp.app"
+// 	}
+
+// 	log.SetLevel(log.DebugLevel)
+// 	log.Info("Starting Jepp API server...")
+
+// 	srv := server.NewServer()
+// 	if err := srv.Router.Run(":8880"); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ecshreve/jepp
 go 1.20
 
 require (
+	github.com/99designs/gqlgen v0.17.34
 	github.com/benbjohnson/clock v1.3.5
 	github.com/gin-gonic/gin v1.9.1
 	github.com/jmoiron/sqlx v1.3.5
@@ -13,12 +14,14 @@ require (
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.1
+	github.com/vektah/gqlparser/v2 v2.5.6
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -34,6 +37,8 @@ require (
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.3 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
@@ -41,6 +46,7 @@ require (
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
@@ -52,7 +58,7 @@ require (
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
-	golang.org/x/tools v0.7.0 // indirect
+	golang.org/x/tools v0.9.3 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
